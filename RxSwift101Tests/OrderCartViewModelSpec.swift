b333966115f7fae68c63f68b37661599a7cd93e1
Siddarth Kalra <sid@sidkalra.com>
2019-01-09 21:02:27 -0500

    fix: broken tests.

--- RxSwift101Tests/OrderCartViewModelSpec.swift
+++ RxSwift101Tests/OrderCartViewModelSpec.swift
@@ -29,7 +29,7 @@ class OrderCartViewModelSpec: QuickSpec {
 
             it("has empty order item") {
                 SharingScheduler.mock(scheduler: scheduler) {
-                    let orderItemEvents = scheduler.record(source: sut.orderItemDriver)
+                    let orderItemEvents = scheduler.record(source: sut.orderItemsDriver)
                     scheduler.start()
 
                     expect(orderItemEvents.firstElement!).to(beEmpty())
@@ -37,23 +37,23 @@ class OrderCartViewModelSpec: QuickSpec {
             }
         }
 
-        describe("orderItemRelay") {
+        describe("orderItemsRelay") {
 
             it("emits events") {
                 let disposeBag = DisposeBag()
                 SharingScheduler.mock(scheduler: scheduler) {
-                    let orderItemEvents = scheduler.record(source: sut.orderItemDriver)
+                    let orderItemEvents = scheduler.record(source: sut.orderItemsDriver)
 
                     scheduler
                         .createHotObservable([next(0, "test1"), next(0, "test2"), next(0, "test3")])
-                        .bind(to: sut.orderItemRelay).disposed(by: disposeBag)
+                        .bind(to: sut.newMenuItemRelay).disposed(by: disposeBag)
 
                     scheduler.start()
 
                     expect(orderItemEvents.events[0].value.element).to(beEmpty())
-                    expect(orderItemEvents.events[1].value.element).to(equal("test1"))
-                    expect(orderItemEvents.events[2].value.element).to(equal("test2"))
-                    expect(orderItemEvents.events[3].value.element).to(equal("test3"))
+                    expect(orderItemEvents.events[1].value.element).to(equal(["test1"]))
+                    expect(orderItemEvents.events[2].value.element).to(equal(["test1", "test2"]))
+                    expect(orderItemEvents.events[3].value.element).to(equal(["test1", "test2", "test3"]))
                 }
             }
         }
